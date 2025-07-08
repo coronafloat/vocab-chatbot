cfd258bca5bc3f9d58de2dc1b2f7c447ebf21599
Emmanuel <e.manggalanusa@gmail.com>
2025-07-08 21:48:48 +0700

    delete tagger

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ nltk.download('wordnet')
 nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('averaged_perceptron_tagger_eng')
-nltk.download('averaged_perceptron_tagger', quiet=True)
 
 stop_words = set(stopwords.words('english'))
 
