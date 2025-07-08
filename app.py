fb63dd65b21fd24bdb4f2b98c4de020a0f9e3bed
Emmanuel <e.manggalanusa@gmail.com>
2025-07-08 20:27:58 +0700

    fix error change downloader to top

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger_eng')
 import streamlit as st
 import nltk
 from nltk.tokenize import word_tokenize
@@ -5,11 +10,6 @@ from nltk import pos_tag
 from utils.vocal_helper import get_word_info
 from nltk.corpus import stopwords
 import time
-nltk.download('punkt_tab')
-# nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger_eng')
 
 stop_words = set(stopwords.words('english'))
 
